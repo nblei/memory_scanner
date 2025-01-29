84928a6a538c2d5d45877e2dc0fa2bc9c7bbcb43
Nathaniel Bleier <nathaniel.bleier@illinois.edu>
2025-01-29 09:53:45 -0500

    Checks for TID

--- src/injector.cc
+++ src/injector.cc
@@ -3,23 +3,26 @@
 #include <csignal>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <ostream>
-#include <unordered_map>
+#include <thread>
 
 extern "C" void __attribute__((constructor)) attach_scanner() {
   struct sigaction sa = {};
   sa.sa_handler = [](int) {
     using time_type = decltype(std::chrono::high_resolution_clock::now());
-    static std::unordered_map<pid_t, time_type> s_ids;
+    static std::map<std::tuple<pid_t, std::thread::id>, time_type> s_ids;
     memory_tools::PointerScanner scanner;
 
     // Get start time of process
     pid_t pid = getpid();
+    std::thread::id tid = std::this_thread::get_id();
+    auto key = std::make_pair(pid, tid);
     time_type start_time;
-    auto it = s_ids.find(pid);
+    auto it = s_ids.find(key);
     if (s_ids.end() == it) {
       start_time = std::chrono::high_resolution_clock::now();
-      s_ids.insert(std::make_pair(pid, start_time));
+      s_ids.insert(std::make_pair(key, start_time));
     } else {
       start_time = it->second;
     }
@@ -34,9 +37,12 @@ extern "C" void __attribute__((constructor)) attach_scanner() {
       (void)addr;
       (void)value;
     });
+
     auto stats = scanner.GetLastScanStats();
     log << std::dec << std::endl
-        << "PID " << pid << " @" << delta << ":" << std::endl
+        << "PID: " << pid << std::endl
+        << "TID: " << tid << std::endl
+        << "@ " << delta << ":" << std::endl
         << stats << std::endl;
   };
   sigaction(SIGUSR1, &sa, nullptr);
