d6d7c80243c7e277017e83497fdf12bc3e27cbc2
Nathaniel Bleier <nathaniel.bleier@illinois.edu>
2025-02-01 17:46:42 -0500

    Made signal handler async-signal-safe

--- src/injector.cc
+++ src/injector.cc
@@ -7,43 +7,78 @@
 #include <ostream>
 #include <thread>
 
-extern "C" void __attribute__((constructor)) attach_scanner() {
-  struct sigaction sa = {};
-  sa.sa_handler = [](int) {
-    using time_type = decltype(std::chrono::high_resolution_clock::now());
-    static std::map<std::tuple<pid_t, std::thread::id>, time_type> s_ids;
-    memory_tools::PointerScanner scanner;
+namespace {
+std::atomic<bool> g_scan_requested{false};
+std::atomic<bool> g_scanner_running{false};
 
-    // Get start time of process
-    pid_t pid = getpid();
-    std::thread::id tid = std::this_thread::get_id();
-    auto key = std::make_pair(pid, tid);
-    time_type start_time;
-    auto it = s_ids.find(key);
-    if (s_ids.end() == it) {
-      start_time = std::chrono::high_resolution_clock::now();
-      s_ids.insert(std::make_pair(key, start_time));
-    } else {
-      start_time = it->second;
+// Async-signal-safe handler that just sets a flag
+void signal_handler(int) {
+  g_scan_requested.store(true, std::memory_order_release);
+}
+
+// Worker thread function that performs the actual scanning
+void scanner_worker() {
+  // Pre-allocate scanner instance outside signal context
+  memory_tools::PointerScanner scanner;
+
+  while (g_scanner_running.load(std::memory_order_acquire)) {
+    if (g_scan_requested.load(std::memory_order_acquire)) {
+      // Get timestamp before scan
+      auto start_time = std::chrono::steady_clock::now();
+
+      // Perform scan
+      scanner.ScanForPointers([](uint64_t /* addr */, uint64_t /* value */) {});
+
+      // Get time delta
+      auto now = std::chrono::steady_clock::now();
+      auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(
+          now - start_time);
+
+      std::ofstream log("memory_scan.log", std::ios::app);
+      auto stats = scanner.GetLastScanStats();
+      log << std::dec << std::endl
+          << "PID: " << getpid() << std::endl
+          << "TID: " << std::this_thread::get_id() << std::endl
+          << "@ " << delta << ":" << std::endl
+          << stats << std::endl;
+
+      // Reset scan request flag
+      g_scan_requested.store(false, std::memory_order_release);
     }
 
-    // Get time delta
-    time_type now = std::chrono::high_resolution_clock::now();
-    auto delta =
-        std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time);
+    // Sleep briefly to avoid spinning
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+}
 
-    std::ofstream log("memory_scan.log", std::ios::app);
-    scanner.ScanForPointers([&](uint64_t addr, uint64_t value) {
-      (void)addr;
-      (void)value;
-    });
+// RAII helper to manage scanner thread lifecycle
+class ScannerThread {
+public:
+  ScannerThread() {
+    g_scanner_running.store(true, std::memory_order_release);
+    thread_ = std::thread(scanner_worker);
+  }
 
-    auto stats = scanner.GetLastScanStats();
-    log << std::dec << std::endl
-        << "PID: " << pid << std::endl
-        << "TID: " << tid << std::endl
-        << "@ " << delta << ":" << std::endl
-        << stats << std::endl;
-  };
+  ~ScannerThread() {
+    g_scanner_running.store(false, std::memory_order_release);
+    if (thread_.joinable()) {
+      thread_.join();
+    }
+  }
+
+private:
+  std::thread thread_;
+};
+
+// Static instance to manage thread lifetime
+static ScannerThread g_scanner_thread;
+
+} // namespace
+
+extern "C" void __attribute__((constructor)) attach_scanner() {
+  struct sigaction sa = {};
+  sa.sa_handler = signal_handler;
+  sa.sa_flags = SA_RESTART; // Restart interrupted system calls
+  sigemptyset(&sa.sa_mask);
   sigaction(SIGUSR1, &sa, nullptr);
 }
